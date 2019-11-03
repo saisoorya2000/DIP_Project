eb1432153fc97775703099d18eb660195b76a7cd
saisoorya <sai.soorya@students.iiit.ac.in>
2019-11-03 22:30:17 +0530

    updated morphing module

--- morphing.m
+++ morphing.m
@@ -1,4 +1,4 @@
-function imDst = morph(imSrc, ptSrc, ptDst)
+function imDst = morphing(imSrc, ptSrc, ptDst)
 
 nCtrlPt = length(ptSrc);
 Xinv = zeros(size(imSrc,1), size(imSrc,2));
@@ -31,5 +31,46 @@ for ki = 1:nCtrlPt
         A(ki, kj) = 1/sqrt(C(kj)+(X1-X2).^2+(Y1-Y2).^2);
     end
 end
+
+A(1:nCtrlPt, nCtrlPt+(1:2)) = ptDst(1:nCtrlPt, 1:2);
+A(1:nCtrlPt, nCtrlPt+3) = 1;
+A(nCtrlPt+(1:2), 1:nCtrlPt) = ptDst(1:nCtrlPt, 1:2).';
+A(nCtrlPt+3, 1:nCtrlPt) = 1;
+Bx(1:nCtrlPt, 1) = ptSrc(1:nCtrlPt, 1);
+By(1:nCtrlPt, 1) = ptSrc(1:nCtrlPt, 2);
+
+AlphaX = A\Bx;
+AlphaY = A\By;
+
+for ki = 1:size(imSrc, 1)
+    tmp = zeros(1, nCtrlPt+3);
+    tmp(nCtrlPt+3) = 1;    
+    tmp(nCtrlPt+2) = ki-1;
+    Xinv_kj = zeros(1,size(imSrc, 2));
+    Yinv_kj = zeros(1,size(imSrc, 2));
+    ptKi_C = (ptDst(:, 2)-ki-1).^2+C;
+    for kj = 0:size(imSrc, 2)-1
+        for n = 1:nCtrlPt
+            tmp(n) = 1/sqrt((ptDst(n, 1)-kj)^2+ptKi_C(n));
+        end
+        tmp(nCtrlPt+1) = kj;
+        Xinv_kj(kj+1) = tmp*AlphaX;
+        Yinv_kj(kj+1) = tmp*AlphaY;
+    end
+    Xinv(ki, :) = Xinv_kj;
+    Yinv(ki, :) = Yinv_kj;
 end
+
+imDst = zeros(size(imSrc));
+[X0, Y0] = meshgrid(1:size(imSrc,2), 1:size(imSrc,1));
+
+for j = 1:size(imSrc,3)
+    tmp = interp2(X0, Y0, imSrc(:,:,j), Xinv, Yinv);
+    tmp(isnan(tmp)) = 0;
+    imDst(:,:,j) = tmp;
+end
+
+end
+
+
 
--- relight.m
+++ relight.m
@@ -2,11 +2,35 @@
 function relight(img, pt, imgRefA, ptRefA)
     % morphing the reference images
     scaleA = max(size(img,1)/size(imgRefA,1),size(img,2)/size(imgRefA,2));
-    imgRefA = imresize(imgRefA, scaleA);    ptRefA = ptRefA*scaleA;
+    imgRefA = imresize(imgRefA, scaleA);    
+    ptRefA = ptRefA*scaleA;
     try
         imgRefA = rgb2gray(imgRefA);
     catch
     end
-%     imgRefA = morph(imgRefA, ptRefA, pt); 
-%     imgRefA = imgRefA(1:size(img,1), 1:size(img,2)); 
-end
+    imgRefA = morphing(imgRefA, ptRefA, pt); 
+    imgRefA = imgRefA(1:size(img,1), 1:size(img,2));
+    
+    % create the mask of convex hull of pt
+    [X,Y] = meshgrid(1:size(img, 2), 1:size(img, 1));
+    ch = convhulln(pt);
+    mask = inpolygon(X,Y,pt(ch(:,1),1),pt(ch(:,1),2));
+    margin = 5;
+    xpro = sum(mask, 1); ypro = sum(mask, 2);
+    left = max(find(xpro>0, 1 )-margin, 0); right = min(find(xpro>0, 1, 'last' )+margin, size(mask, 2));
+    up = max(find(ypro>0, 1 )-margin, 0); down = min(find(ypro>0, 1, 'last' )+margin, size(mask, 1));
+    rect = [left, up, right-left+1, down-up+1];
+    
+    mask = imcrop(mask, rect);
+    
+    imgRefA = imcrop(imgRefA, rect).*mask; 
+    hsv_image = rgb2hsv(imcrop(img, rect));
+    hsv_image(:,:,1) = hsv_image(:,:,1).*mask;
+    hsv_image(:,:,2) = hsv_image(:,:,2).*mask;
+    hsv_image(:,:,3) = hsv_image(:,:,3).*mask;
+    img_crop = hsv2rgb(hsv_image);
+%     figure(2);
+%     imshow(img_crop(:,:,1:2));title('Value Layer');
+   
+    pt(:,1)=pt(:,1)-left;
+    pt(:,2)=pt(:,2)-up;
