4e744a1393774b287d3cc5a00dfb306df81cbb49
saisoorya <sai.soorya@students.iiit.ac.in>
2019-11-23 21:56:01 +0530

    added demo run file

---
+++ demo.m
@@ -0,0 +1,43 @@
+% Demonstrate face image relighting
+%
+% Chen, Xiaowu, et al. "Face illumination transfer through edge-preserving filters." Computer Vision and Pattern Recognition (CVPR), 2011 IEEE Conference on. IEEE, 2011.
+% 
+
+
+close all;
+
+% Input 
+source_name = 'ss1';
+ref_name = 'ref1';
+
+% Reading Images and respective Facial Landmark data
+img=  imresize(im2double(imread(['nimg/' source_name '.PNG'])), 1);
+shape_pt = load(['nimg/' source_name '.mat']);
+shape_pt =double(shape_pt.shape);
+
+imgA=  imresize(im2double(imread(['nimg/' ref_name '.PNG'])), 1);
+shape = load(['nimg/' ref_name '.mat']);
+shape =double(shape.shape);
+
+% Light Transfer 
+results=relight( ...
+        img, ...
+        shape_pt, ...
+        imgA, ...
+        shape ...
+        );
+    
+% Display Results
+results(results>1)=1;
+% result = (results-min(results(:)))./(max(results(:))-min(results(:)));
+results(results<0) = 0;
+figure(1);
+subplot(131);imshow(img);title('Source');
+subplot(132);imshow(imgA);title('Reference');
+subplot(133);imshow(results);title('Output');
+
+% results(results~=0)
+% Saving Result
+%results./(max(results)
+imwrite(results,[source_name '_' ref_name '_ours2.png']);
+
